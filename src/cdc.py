b1cf62e8da851f77d1679137d1f4a70140fa777e
Samuele Valente <samuelevalente@MacBook-Pro.local>
2020-11-06 16:54:32 +0100

    create send_to_dl function

---
+++ src/cdc.py
@@ -0,0 +1,37 @@
+from abc import ABC, abstractmethod
+import os
+import glob
+
+
+class CDC(ABC):
+
+    def __init__(self, data_lake, data_base, arch_type):
+        self.arch_typee=arch_type
+        self.files_path='tmp/'
+        self.data_lake=data_lake
+        self.data_base=data_base
+
+    def send_to_dl(self, ):
+        while(True):
+            list_file = os.listdir(self.files_path)
+
+            for change in list_file:
+                with open(change, 'r') as f:
+                    self.data_lake.write(str.encode(f.read()), '{}.tmp'.format(change.split('.')[0]), 'w')
+
+            cnt=0
+            for f in list_file:
+                if self.data_lake.ls('{}.tmp'.format(change.split('.')[0])): cnt+=1
+
+            if cnt!=len(list_file):
+                for f in list_file:
+                    self.data_lake.remove('{}.tmp'.format(change.split('.')[0]))
+            else:
+                files = glob.glob(self.files_path)
+                for f in files:
+                    os.remove(f)
+                break
+
+    def capture_changes(self, ):
+        pass
+
